590b30cff13a5f401ba34346c060b9c18c6a9586
Chunjie <czhu@vmware.com>
2012-03-29 08:11:31 -0700

    Add new metric collect handlers.
    Change-Id: I9fefe42d726e81aacbae905c7326ceb16dde3de2

---
+++ collector/lib/collector/handlers/backup_manager.rb
@@ -0,0 +1,26 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module Collector
+  class Handler
+    class BackupManager < Handler
+      register BACKUP_MANAGER
+
+      def process(varz)
+        total_size = varz["disk_total_size"] || 0
+        free_size = varz["disk_available_size"] || 0
+        free_percent = 0
+        if (total_size != 0)
+          free_percent = free_size.to_f / total_size.to_f * 100
+        end
+        send_metric("services.nfs_free_space", free_percent,
+                    {:service_type => service_type,
+                     :index => @index})
+      end
+
+      def service_type
+        "backup_manager"
+      end
+
+    end
+  end
+end
