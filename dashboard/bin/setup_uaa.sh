997da7d68f5c53d5f03f492d7812d748d0022bae
Vidya Valmikinathan <vidya@rbcon.com>
2012-10-03 16:54:31 -0700

    [cfid-12] Enable OAuth support
    An UAA server instance (version 1.2.2 or later) is now required to successfully login and access any resource
    on the dashboard app.
    
    Pivotal story under 'Identity' project tracking this feature - [#35352711]
    
    Change-Id: I10e1d735943bdbbfb4dedeed8a4bd71d15d55205

---
+++ dashboard/bin/setup_uaa.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+# if you are running both dashboard and UAA servers locally,
+# use this script to quickly bootstrap UAA with the resources required by dashboard,
+# including one user account for testing purposes
+
+uaac target http://localhost:8081/uaa
+uaac token client get admin --secret adminsecret
+uaac client add dashboard --access_token_validity 5000 --refresh_token_validity 10000 --secret secret --authorized_grant_types "authorization_code,refresh_token" --authorities "uaa.resource,tokens.read,tokens.write" --scope "openid,dashboard.user"
+uaac client add dashboard_admin --access_token_validity 5000 --refresh_token_validity 10000 --secret somesecret --authorized_grant_types "client_credentials" --authorities "scim.read,scim.write,tokens.read,tokens.write,clients.read,clients.write,uaa.admin" --scope "uaa.none"
+uaac token client get dashboard_admin --secret somesecret
+uaac group add openid
+uaac group add dashboard.user
+uaac user add duser1 --email user1@dashboard.org --given_name user1 --family_name test --password pwd1
+uaac member add openid duser1
+uaac member add dashboard.user duser1
+
+echo
+echo "<================================>"
+echo "created test user account"
+echo "username: duser1, password: pwd1"
+uaac user get duser1
+echo "<================================>"
